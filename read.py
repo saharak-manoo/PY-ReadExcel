01230211b9ac5197fdacaa839f6ccf43ede68a17
Saharak <Saharak@bananacoding.com>
2020-03-11 10:01:00 +0700

    [Added] read or editer excel files

--- read.py
+++ read.py
@@ -1,4 +1,17 @@
+import xlsxwriter
 import pandas as pd
- 
-dataframe = pd.read_excel (r'fruits.xlsx')
-print (dataframe)
+
+readDataframe = pd.read_excel(r'fruits.xlsx')
+newDataframe = pd.DataFrame(
+    {'Name': ['orange'], 'Price': [88], 'Amount': [15]}
+)
+
+frames = [readDataframe, newDataframe]
+result = pd.concat(frames)
+
+writer = pd.ExcelWriter('fruits.xlsx', engine='xlsxwriter')
+
+result.to_excel(writer, index=False)
+writer.save()
+
+print(readDataframe)
